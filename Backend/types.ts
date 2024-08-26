0cb86e28c51c811c044319485de45d5d1140c79d
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-08-26 12:28:59 -0700

    add interface nfl data

--- Backend/types.ts
+++ Backend/types.ts
@@ -2,3 +2,42 @@ export type ErrorMessage = string;
 export type ErrorStatus = number;
 export type espnAPIData = Record<string, any>;
 export type espnEvents = Record<string, any>[];
+
+export interface EspnNFLEventResponse {
+  events: EspnNFLEvent[];
+}
+
+export interface EspnNFLEvent {
+  id: string;
+  date: string;
+  name: string;
+  shortName: string;
+  season: {
+    year: number;
+    displayName: string;
+  };
+  competitions: EspnNFLCompetition[];
+}
+export interface EspnNFLCompetition {
+  id: string;
+  date: string;
+  venue: {
+    fullName: string;
+    address: {
+      city: string;
+      state: string;
+      zipCode: string;
+    };
+  };
+  status: {
+    type: {
+      id: string;
+      name: string;
+      state: string;
+      completed: boolean;
+      description: string;
+      detail: string;
+      shortDetail: string;
+    };
+  };
+}
