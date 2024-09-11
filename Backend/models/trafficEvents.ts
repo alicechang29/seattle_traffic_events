84853995cfee6e440b3cf4f083145f9a31063e32
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-09-10 20:56:50 -0400

    add updateEvents

--- Backend/models/trafficEvents.ts
+++ Backend/models/trafficEvents.ts
@@ -1,86 +1,132 @@
-// import db from "../db.ts";
+import db from "../db.ts";
 import { BadRequestError, NotFoundError } from "../expressError.ts";
-import type { tTrafficEvent } from "../types.ts";
-
+import type { espnEvent } from "../types.ts";
+import { sqlForPartialUpdate } from "../helpers/sqlForPartialUpdate.ts";
 export class TrafficEvent {
 
+  /**Create an event (from data), update db, return new event data.
+   *
+   * Data should be {name,startDate,shortName,statusValue,statusCompleted,venue,zipcode}
+   *
+   * Returns {name,startDate,shortName,statusValue,statusCompleted,venue,zipcode}
+  */
   static async create(
     { name,
       startDate,
-      endDate,
       shortName,
       statusValue,
-      statusComplete,
-      venue
-    }: tTrafficEvent) {
+      statusCompleted,
+      venue,
+      zipcode,
+    }: espnEvent) {
 
     console.log("models/TrafficEvent/create");
 
     const duplicateCheck = await db.query(`
-        SELECT name, start_date
+        SELECT id, name, start_date
         FROM traffic_events
-        WHERE name = $1 AND date = $2`, [name, startDate]);
+        WHERE name = $1 AND start_date = $2`, [name, startDate]);
 
-    if (duplicateCheck.rows[0])
-      throw new BadRequestError(`Duplicate traffic event: ${name} - ${startDate}`);
+    //TODO: check if this is returning an ID
+    if (duplicateCheck.rows[0]) {
+      const dupeEvent = duplicateCheck.rows[0];
+      throw new BadRequestError(`${dupeEvent.id}`);
+    }
 
     const result = await db.query(`
                 INSERT INTO traffic_events (
                 name,
                 start_date,
-                end_date,
                 short_name,
                 status_value,
-                status_complete,
-                venue_name)
+                status_completed,
+                venue,
+                zipcode)
                 VALUES ($1, $2, $3, $4, $5, $6, $7)
                 RETURNING
+                    id,
                     name,
                     start_date AS startDate,
                     end_date AS endDate,
                     short_name AS shortName,
                     status,
-                    venue`, [
-      name, startDate, endDate, shortName, statusValue, statusComplete, venue
+                    venue,
+                    zipcode`, [
+      name, startDate, shortName, statusValue, statusCompleted, venue, zipcode
     ],
     );
     const trafficEvent = result.rows[0];
 
     return trafficEvent;
   }
-
+  /**Gets all events.
+    * Returns [{name,startDate,shortName,statusValue,statusCompleted,venue,zipcode},...]
+   */
   static async getEvents() {
     console.log("models/TrafficEvent/getEvents");
     const result = await db.query(`
-      SELECT name,
+      SELECT id,
+              name,
               start_date AS startDate,
-              end_date AS endDate,
               short_name AS shortName,
               status_value AS statusValue,
-              status_complete AS statusComplete,
-              venue_name AS venue
+              status_completed AS statusCompleted,
+              venue,
+              zipcode
       FROM traffic_events`
     );
 
     return result.rows;
   }
 
-  //TODO: update statuses later to be dynamic
+  /**Gets all events that have a status of scheduled or in progress.
+   *
+   * Returns [{name,startDate,shortName,statusValue,statusCompleted,venue,zipcode},...]
+  */
   static async getScheduledInProgressEvents() {
     console.log("models/TrafficEvent/getScheduledInProgressEvents");
 
     const result = await db.query(`
-      SELECT name,
+      SELECT id,
+              name,
               start_date AS startDate,
-              end_date AS endDate,
               short_name AS shortName,
               status_value AS statusValue,
-              status_complete AS statusComplete,
-              venue_name AS venue
+              status_completed AS statusCompleted,
+              venue,
+              zipcode
       FROM traffic_events
       WHERE status_value IN ($1, $2)`, ['STATUS_SCHEDULED', 'STATUS_IN_PROGRESS']
     );
 
     return result.rows;
+  }
+
+  /**Update event data with 'data'.
+   * Data can include {name,startDate,shortName,statusValue,statusCompleted,venue,zipcode}
+   *
+   * Returns {name,startDate,shortName,statusValue,statusCompleted,venue,zipcode}
+   *
+   * Throws NotFoundError if not found.
+   */
+  static async updateEvent(eventId: number, data: espnEvent): Promise<any> {
+    console.log("updateEvent");
+
+    const { setCols, values } = sqlForPartialUpdate(
+      data,
+      {
+        startDate: "start_date",
+        shortName: "short_name",
+        statusValue: "status_value",
+        statusCompleted: "status_completed",
+        venueName: "venue_name"
+      }
+    );
+
+    const updateSqlQuery = `
+      UPDATE events
+      SET ${setCols}
+      WHERE id = ${eventId}
+    `;
   }
 }
