0396ec8adb74735ec961a7e78ca729d023606d0d
alicechang29 <147457604+alicechang29@users.noreply.github.com>
2024-10-31 23:08:08 -0400

    notes

--- Backend/routes/trafficEvents.ts
+++ Backend/routes/trafficEvents.ts
@@ -1,8 +1,6 @@
-import { parseNFLData } from "../helpers/parseNFLData";
-import { TrafficEvent } from "../models/trafficEvents";
 import { BadRequestError } from "../expressError";
 import { Router } from "express";
-import { getSeahawksData } from "../controllers/trafficEvents";
+import { fetchEventDataByStartDateAndStatus } from "../controllers/trafficEvents";
 
 const router = Router();
 
@@ -12,21 +10,21 @@ const router = Router();
 router.get("/today", async (req, res) => {
   console.log("GET/ today's events");
 
-  //2024-09-03T18:21:03.531Z --> '2024-09-03'
-  const today = new Date().toISOString().split('T')[0]; // Format YYYY-MM-DD
-  const startDate = new Date(today);
+  // FIXME: the dates should come in from the request
 
-  const endDate = new Date(today);
-  endDate.setDate(startDate.getDate() + 1);
+
+
+  const status = ['STATUS_SCHEDULED', 'STATUS_IN_PROGRESS'];
 
   try {
-    const seahawksData = await getSeahawksData(req, res, startDate, endDate);
-    //TODO: in the future when there is more event data, controller handles putting all events for today together
-    return res.status(201).json({ seahawksData });
+    //TODO: need to finish this
+    const data = await fetchEventDataByStartDateAndStatus(req, res);
+
+    return res.status(201).json({ data });
 
   } catch (e) {
     console.error("Issue with fetching today's events", e);
-    // throw new BadRequestError();
+    throw new BadRequestError();
   }
 
 });
